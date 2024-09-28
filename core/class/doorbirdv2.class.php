6b743c6e0e3a0a40d937a9c78450df1c53b6866f
vegeta0911 <67876957+vegeta0911@users.noreply.github.com>
2024-09-28 22:47:50 +0200

    Update doorbirdv2.class.php

--- core/class/doorbirdv2.class.php
+++ core/class/doorbirdv2.class.php
@@ -240,7 +240,7 @@ class doorbirdv2 extends eqLogic {
             $urlLive = 'http://' . $ip . '/bha-api/video.cgi?sessionid='.$api;
             $form = '<img style="display: block;-webkit-user-select: none;margin: auto;cursor: zoom-in;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src='.$urlLive . ' width="324" height="243">';
             
-            log::add('doorbirdv2', 'debug', 'Camera api : ' . $api . ' avec ' . $urlLive . ':' . $pass);
+            log::add('doorbirdv2', 'debug', 'Camera api : ' . $api . ' avec ' . $urlLive);
             $eqLogic->checkAndUpdateCmd('path_url_live', $form);
             $eqLogic->refreshWidget();
         }
